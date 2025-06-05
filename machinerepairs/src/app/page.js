6b722fd888a34d0a48feb016ee10d0548e26328c
Will Taylor <willtaylor2352@gmail.com>
2025-06-05 09:37:20 -0400

    Edited HomePage

--- machinerepairs/src/app/page.js
+++ machinerepairs/src/app/page.js
@@ -2,12 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<div className="grid min-h-[calc(100vh-50px-24px)] grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-			<main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-				<div>
-					<p>this is my body</p>
-				</div>
-			</main>
-		</div>
+		<main className="min-h-[calc(100vh-250px)]">
+			<div className="flex flex-col">
+				<h1>What are you trying to do?</h1>
+			</div>
+		</main>
 	);
 }
