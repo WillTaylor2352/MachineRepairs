c6ad155972775b8d1d865983232921f084b5939d
Will Taylor <willtaylor2352@gmail.com>
2025-06-06 09:41:35 -0400

    Theme Switcher component and integration
    Added a theme switcher component and assets. Integrated into Navigation component.

---
+++ machinerepairs/src/components/client/ThemeSwitcher.js
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+export default function ThemeSwitcher(props) {
+	const imagePadding = 'p-3';
+	const [theme, setTheme] = useState(null);
+	function handleClick() {
+		const newTheme = theme === 'light' ? 'dark' : 'light';
+		setTheme(newTheme);
+		if (typeof window !== undefined) {
+			window.localStorage.setItem('currentTheme', newTheme);
+		}
+	}
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			let currentTheme = window.localStorage.getItem('currentTheme');
+			if (!currentTheme) {
+				console.log('hello there');
+				currentTheme = 'light';
+				window.localStorage.setItem('currentTheme', currentTheme);
+			}
+			setTheme(currentTheme);
+		}
+	}, []);
+
+	return (
+		<div className={`flex min-w-16 max-w-24 items-center justify-center`}>
+			{(theme === 'light' && (
+				<button className={`${imagePadding}`} onClick={handleClick}>
+					<Image
+						src="/sun.svg"
+						width={25}
+						height={25}
+						alt="Switch to Light Mode"
+					/>
+				</button>
+			)) || (
+				<button className={`${imagePadding}`} onClick={handleClick}>
+					<Image
+						src="/moon.svg"
+						width={25}
+						height={25}
+						alt="Switch to Dark Mode"
+					/>
+				</button>
+			)}
+		</div>
+	);
+}
--- machinerepairs/src/components/layout/Navigation.js
+++ machinerepairs/src/components/layout/Navigation.js
@@ -1,10 +1,12 @@
 import Nav_Buttons from '../client/Navigation_buttons';
+import ThemeSwitcher from '../client/ThemeSwitcher';
 const li = 'm-4';
-const ul = 'w-full inline-flex justify-center';
+const ul = 'inline-flex justify-center clear-end';
 
 export default function Navigation() {
 	return (
-		<div className="w-full">
+		<div className="mt-3 flex justify-evenly">
+			<span className="min-w-16 max-w-24"></span>
 			<ul className={`${ul}`}>
 				<li className={`${li}`}>
 					<Nav_Buttons title={'Home'} link={'/'} />
@@ -19,6 +21,7 @@ export default function Navigation() {
 					<Nav_Buttons title={'Repair Tech'} link={'/RepairTech'} />
 				</li>
 			</ul>
+			<ThemeSwitcher li={li} />
 		</div>
 	);
 }
