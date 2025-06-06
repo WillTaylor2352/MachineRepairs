ed1f1a2b8450fe92647202ef19a99c57bb0e444d
Will Taylor <willtaylor2352@gmail.com>
2025-06-06 10:13:04 -0400

    Moved main tag, added controls
    Moved the main tag to the root layout so it loads on every page for better indexing. Added controls and began blocking out the home page for navigation.

--- machinerepairs/src/app/layout.js
+++ machinerepairs/src/app/layout.js
@@ -28,7 +28,9 @@ export default function RootLayout({ children }) {
 						<Navigation className="" />
 					</div>
 				</header>
-				{children}
+				<main className="h-[calc(100vh-20vh)] min-h-[calc(100vh-30vh)]">
+					{children}
+				</main>
 				<footer className="row-start-3 flex h-[50px] flex-wrap items-center justify-center gap-4">
 					<div>
 						<p>footer goes here</p>
--- machinerepairs/src/app/page.js
+++ machinerepairs/src/app/page.js
@@ -1,11 +1,28 @@
+import Button from '@/components/client/Button';
 import Image from 'next/image';
 
 export default function Home() {
+	const defaultstyle = 'p-4';
+	const buttonItemSpacing = 'mt-3';
 	return (
-		<main className="min-h-[calc(100vh-250px)]">
-			<div className="flex flex-col">
-				<h1>What are you trying to do?</h1>
+		<div className="item flex h-full flex-col items-center">
+			<div className="m-auto flex w-1/2 flex-col items-center pb-[15vh] text-center">
+				<h1
+					className={`${defaultstyle} mb-[calc(5vh)] border-b-[1px] text-3xl`}
+				>
+					What are you trying to do?
+				</h1>
+				<Button
+					className={`${defaultstyle} `}
+					description={'Check in Machine'}
+				/>
+				<Button
+					classes={`${defaultstyle} ${buttonItemSpacing} `}
+					description={'Transfer Machine'}
+					overrideClasses={true}
+				/>
+				<Button className={`${defaultstyle}`} description={'Work on Machine'} />
 			</div>
-		</main>
+		</div>
 	);
 }
