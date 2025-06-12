99a153a5710fd1598db5872e7f78355d2e5f65c2
Will Taylor <willtaylor2352@gmail.com>
2025-06-12 09:18:36 -0400

    Styling updates

--- machinerepairs/src/app/page.js
+++ machinerepairs/src/app/page.js
@@ -2,26 +2,32 @@ import Button from '@/components/client/Button';
 import Image from 'next/image';
 
 export default function Home() {
-	const defaultstyle = 'p-4';
+	const b_defaultstyle = 'p-4';
 	const buttonItemSpacing = 'mt-3';
 	return (
-		<div className="item flex h-full flex-col items-center">
-			<div className="m-auto flex flex-col items-center pb-[15vh] text-center md:w-1/2">
-				<h1
-					className={`${defaultstyle} mb-[calc(5vh)] border-b-[1px] text-3xl`}
-				>
-					What are you trying to do?
+		<div className="flex h-full flex-col justify-center">
+			<div className="flex flex-col justify-center text-center md:w-1/2">
+				<h1 className={`mb-[calc(5vh)] border-b-[1px] p-4 text-3xl`}>
+					Machine Repairs
 				</h1>
-				<Button
-					className={`${defaultstyle} `}
-					description={'Check in Machine'}
-				/>
-				<Button
-					classes={`${defaultstyle} ${buttonItemSpacing} `}
-					description={'Transfer Machine'}
-					overrideClasses={true}
-				/>
-				<Button className={`${defaultstyle}`} description={'Work on Machine'} />
+				<div className="grid grid-cols-2 gap-2">
+					<Button
+						className={`${b_defaultstyle} `}
+						description={'Check in Machine'}
+					/>
+					<Button
+						className={`${b_defaultstyle}`}
+						description={'Transfer Machine'}
+					/>
+					<Button
+						className={`${b_defaultstyle}`}
+						description={'Work on Machine'}
+					/>
+					<Button
+						className={`${b_defaultstyle}`}
+						description={'Close Machine Repair'}
+					/>
+				</div>
 			</div>
 		</div>
 	);
