4a1524910e0e41a4f4018d6cfbd95470b67d8aef
Will Taylor <willtaylor2352@gmail.com>
2025-06-17 16:16:35 -0400

    Create arrow component
    Create component for dynamic arrow component

---
+++ machinerepairs/src/components/server/Arrow.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+export default function Arrow({ width, height, className }) {
+	return (
+		<div className={`flex h-[44px] ${className}`}>
+			<Image
+				src={'/arrow_tail.svg'}
+				width={width}
+				height={height}
+				className="antialiased"
+				alt="An arrow... duh"
+			/>
+			<Image
+				src={'/arrow_body.svg'}
+				width={width}
+				height={height}
+				className="antialiased"
+				alt="An arrow... duh"
+			/>
+			<Image
+				src={'/arrow_point.svg'}
+				width={width}
+				height={height}
+				className="antialiased"
+				alt="An arrow... duh"
+			/>
+		</div>
+	);
+}
