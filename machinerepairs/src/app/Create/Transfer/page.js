2372c0137617f6f3d075760aba48a5c7bc668707
Will Taylor <willtaylor2352@gmail.com>
2025-06-17 16:19:18 -0400

    Add headers to create function pages

--- machinerepairs/src/app/Create/Transfer/page.js
+++ machinerepairs/src/app/Create/Transfer/page.js
@@ -1,3 +1,16 @@
+import Arrow from '@/components/server/Arrow';
+import FunctionPageHeader from '@/components/server/Function_Page_Header';
+import Image from 'next/image';
+
 export default function Page() {
-	return <div>Transfer Create</div>;
+	const arrowSize = 44;
+	const spanClass = 'text-xs text-center';
+	return (
+		<div>
+			<FunctionPageHeader
+				hereText={'Create Transfer'}
+				nextStepText={'Process Transfer'}
+			/>
+		</div>
+	);
 }
