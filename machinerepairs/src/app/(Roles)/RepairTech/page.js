d80ccc0dd06fce276346590dfac39401ed0129db
Will Taylor <willtaylor2352@gmail.com>
2025-06-17 13:38:40 -0400

    Blocked out role based functions

--- machinerepairs/src/app/(Roles)/RepairTech/page.js
+++ machinerepairs/src/app/(Roles)/RepairTech/page.js
@@ -1,3 +1,22 @@
 export default function RepairTech() {
-	return <div>Repairs Tech Stuff</div>;
+	return (
+		<div className="ml-10">
+			<h1>Blocking / ideas</h1>
+			<h2>Flow</h2>
+			<ol className="list-decimal">
+				<li>
+					<p>
+						Accept a machine repair id at the top of the page (or scan a barcode
+						attached to the machine)
+					</p>
+				</li>
+				<li>
+					<p>Display list of issues and prompt for repair notes</p>
+				</li>
+				<li>
+					<p>List the parts used and their quantity</p>
+				</li>
+			</ol>
+		</div>
+	);
 }
