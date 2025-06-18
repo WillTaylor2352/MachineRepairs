fae0a9e1422bb2d27d074db56a5aa6a42521010b
Will Taylor <willtaylor2352@gmail.com>
2025-06-18 15:44:42 -0400

    restructured for alignment
    Added a div with additional flexbox around h1 and span elements. Allows for individual assignment of flexbox widths without the need or complexity of a grid system.

--- machinerepairs/src/components/server/Function_Page_Header.js
+++ machinerepairs/src/components/server/Function_Page_Header.js
@@ -5,28 +5,35 @@ export default function FunctionPageHeader({
 	...params
 }) {
 	const arrowSize = params.arrowSize !== undefined ? params.arrowSize : 44;
+	const flexSet = 'flex md:w-1/3 justify-start';
 	const titleClass =
-		'mw-full flex flex-row md:max-w-[33%] md:flex-col md:text-center lg:max-w-full';
+		'flex w-full flex-row md:flex-col md:text-center self-center';
 	const h1Class =
-		'mb-0 w-3/5 border-b-0 sm:max-w-full md:w-full border-r-2 md:border-r-0';
+		'mb-0 w-3/5 border-b-0 sm:max-w-full md:w-full border-r-2 md:border-r-0 pr-3';
 	const spanClass =
-		'md:text-xs text-center m-auto rounded-2xl text-sm p-3 md:p-0';
+		'p-3 text-center text-sm md:p-0 md:text-xs min-w-fit mx-auto self-center';
 
 	return (
 		<div>
 			<div className="flex flex-col justify-between border-b-[1px] pb-3 md:flex-row">
-				<div className={`${titleClass}`}>
-					<h1 className={`${h1Class} `}>{hereText}</h1>
-					<span className={`${spanClass}`}>You are here</span>
+				<div className={`${flexSet} `}>
+					<div className={`${titleClass}`}>
+						<h1 className={`${h1Class} `}>{hereText}</h1>
+						<span className={`${spanClass}`}>You are here</span>
+					</div>
 				</div>
-				<Arrow
-					width={arrowSize}
-					height={arrowSize}
-					className="relative top-[7px] mb-5 self-start md:top-[-7px] md:mb-0 md:self-center"
-				/>
-				<div className={`${titleClass}`}>
-					<h1 className={`${h1Class}`}>{nextStepText}</h1>
-					<span className={`${spanClass}`}>Next Step</span>
+				<div className={`${flexSet} md:justify-center`}>
+					<Arrow
+						width={arrowSize}
+						height={arrowSize}
+						className="relative top-[7px] mb-5 self-start md:top-[-7px] md:mb-0 md:self-center"
+					/>
+				</div>
+				<div className={`${flexSet} md:justify-end`}>
+					<div className={`${titleClass}`}>
+						<h1 className={`${h1Class}`}>{nextStepText}</h1>
+						<span className={`${spanClass}`}>Next Step</span>
+					</div>
 				</div>
 			</div>
 		</div>
