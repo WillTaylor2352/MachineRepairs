057c5186b88e6eb0c97504e03d7e8b25bac1a883
Will Taylor <willtaylor2352@gmail.com>
2025-06-18 11:00:44 -0400

    Changed note text smaller screen size
    removed the background color and added a right border to the h1 tag

--- machinerepairs/src/components/server/Function_Page_Header.js
+++ machinerepairs/src/components/server/Function_Page_Header.js
@@ -5,17 +5,18 @@ export default function FunctionPageHeader({
 	...params
 }) {
 	const arrowSize = params.arrowSize !== undefined ? params.arrowSize : 44;
-	const spanClass =
-		'text-xs text-center m-auto rounded-2xl bg-slate-500 md:bg-transparent p-3 md:p-0';
-	const h1Class = 'mb-0 w-3/5 border-b-0 sm:max-w-full md:w-full';
 	const titleClass =
 		'mw-full flex flex-row md:max-w-[33%] md:flex-col md:text-center lg:max-w-full';
+	const h1Class =
+		'mb-0 w-3/5 border-b-0 sm:max-w-full md:w-full border-r-2 md:border-r-0';
+	const spanClass =
+		'md:text-xs text-center m-auto rounded-2xl text-sm p-3 md:p-0';
 
 	return (
 		<div>
 			<div className="flex flex-col justify-between border-b-[1px] pb-3 md:flex-row">
 				<div className={`${titleClass}`}>
-					<h1 className={`${h1Class}`}>{hereText}</h1>
+					<h1 className={`${h1Class} `}>{hereText}</h1>
 					<span className={`${spanClass}`}>You are here</span>
 				</div>
 				<Arrow
